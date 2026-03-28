49b24dc91ad5ae353be5e4e3699e4be3a7e138a8
droLz1n <maiapdr99@gmail.com>
2026-03-28 19:19:31 -0300

    Adicionando as operações iniciais para funcionamento

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-from modulos import classes
-# from modulos import funcoes
+from modulos.classes import Estoque, Produto
 
+estoque = Estoque()
 
-cafe_pilao = classes.Produto("Café Pilão Extraforte", "500g", 50, 29.50, 1)
-print(cafe_pilao)
+while True:
+    print("\n1 - Adicionar produto")
+    print("2 - Listar produtos")
+    print("3 - Atualizar produto")
+    print("4 - Remover produto")
+    print("0 - Sair")
+
+    operacao = input("Escolha a operação desejada: ")
+
+    if operacao == "1":
+        nome = input("Nome: ")
+        tamanho = input("Tamanho: ")
+        quantidade = int(input("Quantidade: "))
+        preco = float(input("Preço: "))
+        codigo = input("Código: ")
+
+        produto = Produto(nome, tamanho, quantidade, preco, codigo)
+        estoque.adicionar_produto(produto)
+
+    elif operacao == "2":
+        estoque.listar_produtos()
+
+    elif operacao == "3":
+        codigo = input("Código do produto: ")
+        estoque.atualizar_produto(codigo)
+
+    elif operacao == "4":
+        codigo = input("Código do produto: ")
+        estoque.remover_produto(codigo)
+
+    elif operacao == "0":
+        break
--- modulos/classes.py
+++ modulos/classes.py
@@ -9,3 +9,43 @@ class Produto:
 
     def __str__(self):
         return f'O produto {self.nome} de {self.tamanho}, de código {self.codigo}, possuí {self.quantidade} unidades e está custando {self.preco:.2f} a unidade.'
+
+
+class Estoque:
+
+    def __init__(self):
+        self.produtos = []
+
+    def adicionar_produto(self, produto):
+        self.produtos.append(produto)
+
+    def listar_produtos(self):
+        if not self.produtos:
+            print("Estoque vazio.")
+            return
+        for produto in self.produtos:
+            print(produto)
+
+    def buscar_produto(self, codigo):
+        for produto in self.produtos:
+            if produto.codigo == codigo:
+                return produto
+        return None
+
+    def atualizar_produto(self, codigo):
+        produtos = self.buscar_produto(codigo)
+
+        if produtos:
+            produtos.quantidade = int(input("Nova quantidade: "))
+            produtos.preco = float(input("Nova preço: "))
+        else:
+            print("Produto não encontrado.")
+
+    def remover_produto(self, codigo):
+        produtos = self.buscar_produto(codigo)
+
+        if produtos:
+            self.produtos.remove(produtos)
+            print("Produto removido.")
+        else:
+            print("Produto não encontrado.")
