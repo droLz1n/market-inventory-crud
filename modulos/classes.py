8d00010869d2cbc5db69e38c5ead02983fbe9e80
droLz1n <maiapdr99@gmail.com>
2026-03-28 21:04:30 -0300

    Adicionando um gerador de código automático

--- modulos/classes.py
+++ modulos/classes.py
@@ -15,9 +15,15 @@ class Estoque:
 
     def __init__(self):
         self.produtos = []
+        self.contador_codigo = 1
 
-    def adicionar_produto(self, produto):
+    def adicionar_produto(self, nome, tamanho, quantidade, preco):
+        codigo = self.gerar_codigo()
+
+        produto = Produto(nome, tamanho, quantidade, preco, codigo)
         self.produtos.append(produto)
+
+        print(f"Produto cadastrado com código: {codigo}")
 
     def listar_produtos(self):
         if not self.produtos:
@@ -49,3 +55,11 @@ class Estoque:
             print("Produto removido.")
         else:
             print("Produto não encontrado.")
+
+    def gerar_codigo(self):
+        if self.contador_codigo > 99999:
+            raise ValueError("Limite de códigos atingido(99999).")
+
+        codigo = f"{self.contador_codigo:05d}"
+        self.contador_codigo += 1
+        return codigo
