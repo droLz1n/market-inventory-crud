9f35669175dbdf093038baf277f3da99734dd591
droLz1n <maiapdr99@gmail.com>
2026-04-08 02:42:52 -0300

    Foi adicionado o botão de Início.

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ function carregarPagina(pagina) {
     const content = document.getElementById("content");
 
     switch(pagina) {
+        case 'inicio':
+            content.innerHTML = "<h1>Bem-vindo ao sistema</h1><p>Selecione uma opção no menu.</p>";
+            break;
+
         case 'add':
             content.innerHTML = "<h1>Adicionar Produtos</h1>";
             break;
 
         case 'estoque':
-            content.innerHTML = "<h1>Estoque</h1>";
+            content.innerHTML = "<h1>Estoque atual</h1>";
             break;
 
         case 'remover':
