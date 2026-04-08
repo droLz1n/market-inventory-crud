a64872d8dbf3037064617b189ef27bedb42eafc9
droLz1n <maiapdr99@gmail.com>
2026-04-07 21:02:38 -0300

    Começando a integração com a nova Interface web para usuários.

---
+++ script.js
@@ -0,0 +1,37 @@
+function carregarPagina(pagina) {
+    const content = document.getElementById("content");
+
+    switch(pagina) {
+        case 'add':
+            content.innerHTML = "<h1>Adicionar Produtos</h1>";
+            break;
+
+        case 'estoque':
+            content.innerHTML = "<h1>Estoque</h1>";
+            break;
+
+        case 'remover':
+            content.innerHTML = "<h1>Remover Produtos</h1>";
+            break;
+
+        case 'atualizar':
+            content.innerHTML = "<h1>Atualizar Produtos</h1>";
+            break;
+
+        case 'buscarNome':
+            content.innerHTML = "<h1>Buscar por Nome</h1>";
+            break;
+
+        case 'buscarCodigo':
+            content.innerHTML = "<h1>Buscar por Código</h1>";
+            break;
+
+        default:
+            content.innerHTML = "<h1>Bem-vindo</h1>";
+    }
+}
+
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("close");
+}
